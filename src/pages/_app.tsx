e9f24c7c12e70b98bf6bafd8ba74c5c5d87db2f7
junkor-1011 <tomoyukizxy@gmail.com>
2022-08-23 01:16:35 +0900

    feat: getLayout(wip)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
@@ -5,14 +6,23 @@ import { RecoilRoot } from 'recoil';
 import React from 'react';
 
 import type { AppProps } from 'next/app';
+import type { NextPage } from 'next';
 
-// eslint-disable-next-line arrow-body-style
-const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+type GetLayout = (page: React.ReactElement) => React.ReactNode;
+type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout: GetLayout;
+};
+type AppPropsWithLayout<P = {}> = AppProps<P> & {
+  Component: NextPageWithLayout;
+};
+
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout || ((page) => page);
   return (
     <RecoilRoot>
       {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
       <SessionProvider session={session} refetchInterval={5 * 60}>
-        <Component {...pageProps} />
+        {getLayout(<Component {...pageProps} />)}
       </SessionProvider>
     </RecoilRoot>
   );
